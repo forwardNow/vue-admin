392e01db0285a830850643255302f3ce22b16ea3
forwardNow <wuqinfei_csdn@163.com>
2018-12-30 22:15:35 +0800

    【系统设置】重构菜单树：添加展开选项

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -66,6 +66,7 @@
               :data="menus"
               :show-checkbox="false"
               :default-expand-all="false"
+              :default-expanded-keys="expandedNodeIds"
               node-key="upcId"
               ref="tree"
               highlight-current
@@ -127,6 +128,7 @@
     },
     data() {
       return {
+        items: [],
         activeIndex: null,
         user: {},
         menus: [],
@@ -154,12 +156,12 @@
         });
         */
         const items = [
-          { upcId: 0, upcRightName: '系统设置', fatherId: null, url: '/system', icon: 'iconfont icon-jiaoseguanli', },
+          { upcId: 0, upcRightName: '系统设置', fatherId: null, url: '/system', icon: 'iconfont icon-jiaoseguanli', expand: true },
           { upcId: 1, upcRightName: '机构管理', fatherId: 0, url: '/system/org', icon: '', },
           { upcId: 2, upcRightName: '部门管理', fatherId: 0, url: '/system/dept', icon: '', },
           { upcId: 3, upcRightName: '角色管理', fatherId: 0, url: '/system/role', icon: '', },
           { upcId: 4, upcRightName: '用户管理', fatherId: 0, url: '/system/user', icon: '', },
-          { upcId: 5, upcRightName: '证书管理', fatherId: 0, url: '/system/license', icon: '', },
+          { upcId: 5, upcRightName: '证书管理', fatherId: 0, url: '/system/license', icon: '', expand: true },
           { upcId: 6, upcRightName: '我的证书', fatherId: 5, url: '/system/license/my', icon: '', },
           { upcId: 7, upcRightName: '生成正常', fatherId: 5, url: '/system/license/gen', icon: '', },
           { upcId: 8, upcRightName: '系统日志', fatherId: 0, url: '/system/sysLog', icon: '', },
@@ -167,6 +169,8 @@
           { upcId: 10, upcRightName: '系统状态', fatherId: 0, url: '/system/sysStatus', icon: '', },
           { upcId: 11, upcRightName: '个人中心', fatherId: 0, url: '/system/profile/', icon: '', },
         ];
+
+        this.items = items;
 
         this.menus = TreeUtils.createNestedTree(items, null,
           {
@@ -229,6 +233,9 @@
       breadcrumbRouteList() {
         return this.$store.state.breadcrumbRouteList;
       },
+      expandedNodeIds() {
+        return this.items.filter(item => item.expand).map(item => item.upcId);
+      }
     },
   };
 </script>
